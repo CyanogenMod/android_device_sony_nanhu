a52fb1f1cb3a8df81cae9c521cfe3cb1b0d379c9
Alin Jerpelea <jerpelea@gmail.com>
2013-03-24 12:11:31 +0200

    nanhu: liblight update
    Change-Id: Ia5bdd5f6b2f1b7a6ba4db53f3da4ff1619d623b8

--- include/lights.h
+++ include/lights.h
@@ -1,21 +1,12 @@
-char const*const RED_LED_ON          = "01 0 1";
-char const*const GREEN_LED_ON        = "01 1 1";
-char const*const BLUE_LED_ON         = "01 2 1";
-char const*const RED_LED_OFF         = "01 0 0";
-char const*const GREEN_LED_OFF       = "01 1 0";
-char const*const BLUE_LED_OFF        = "01 2 0";
+#define STANDARD_LIGHTS
 
-char const*const RED_LED_BLINK_ON    = "02 0 1";
-char const*const GREEN_LED_BLINK_ON  = "02 1 1";
-char const*const BLUE_LED_BLINK_ON   = "02 2 1";
-char const*const RED_LED_BLINK_OFF   = "02 0 0";
-char const*const GREEN_LED_BLINK_OFF = "02 1 0";
-char const*const BLUE_LED_BLINK_OFF  = "02 2 0";
+char const*const RED_LED_FILE          = "/sys/class/leds/red/brightness";
+char const*const GREEN_LED_FILE        = "/sys/class/leds/green/brightness";
+char const*const BLUE_LED_FILE         = "/sys/class/leds/blue/brightness";
+char const*const SNS_LED_FILE          = "/sys/class/leds/sns/rgb_brightness";
 
-char const*const KEY_LED_ON          = "01 3 1";
-char const*const KEY_LED_OFF         = "01 3 0";
+char const*const LCD_BACKLIGHT_FILE    = "/sys/class/leds/lcd-backlight/brightness";
+char const*const BUTTON_BACKLIGHT_FILE  = "/sys/class/leds/button-backlight/brightness";
 
-char const*const LCD_BACKLIGHT_FILE  = "/sys/devices/virtual/graphics/fb0/msmfb_bl0/brightness";
 
-char const*const LED_CONTROL_FILE    = "/sys/class/led/fih_led/control";
 

1b3b43dc0116cffc186a0c57c6a1091035b3b689
Alin Jerpelea <jerpelea@gmail.com>
2013-03-24 09:56:31 +0200

    nanhu: remove brcm fw
    Change-Id: I19c44cb8b18d22a58e1096febba53b0558eeb458

--- nanhu.mk
+++ nanhu.mk
@@ -66,7 +66,3 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/sony/nanhu/nanhu-vendor.mk)
 
-# Wifi
-BOARD_WLAN_DEVICE_REV := bcm4330_b1
-WIFI_BAND             := 802_11_ABG
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
